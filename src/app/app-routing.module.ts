b22d345abc9283df5710f92cb09add3d1789f3be
Parvathi Prasath <parvathikmkr@gmail.com>
2022-10-18 18:04:04 -0400

    Category & Course mobule skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryModule } from './category/category.module';
+import { CourseModule } from './course/course.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'category', component: CategoryModule},
+  { path: 'course', component: CourseModule},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/course/course.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListCourseComponent } from './pages/list-course/list-course.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListCourseComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CourseModule { }
