dd8c0db5503edee863763194f430993e9f7a8868
Parvathi Prasath <parvathikmkr@gmail.com>
2022-10-20 15:30:06 -0400

    Test case fixed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('BO-skillarena');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('BO-skillarena app is running!');
-  });
 });
