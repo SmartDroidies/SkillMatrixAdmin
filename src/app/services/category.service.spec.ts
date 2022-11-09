a748de5236c6edc421efe69496bf107e57f6c05e
Parvathi Prasath <parvathikmkr@gmail.com>
2022-11-09 14:37:21 -0500

    testcoverage for services

--- src/app/services/category.service.spec.ts
+++ src/app/services/category.service.spec.ts
@@ -35,6 +35,52 @@ describe('CategoryService', () => {
 
     req.flush(mockCategories);
   });
+
+  it('should call updateCategory and return the updates category from the API', () => {
+    const updateCategory: Category = {
+      code: '',
+      order: 0,
+      title: '',
+      active: false,
+    };
+
+    const mockCategory: Category = {
+      active: true,
+      code: 'fitness',
+      order: 30,
+      title: 'Fitness',
+    };
+
+    service.updateCategory(mockCategory).subscribe((data) => {
+      expect(data).toEqual(updateCategory);
+    });
+
+    const req = httpController.expectOne({
+      method: 'POST',
+      url: '/course-api/category',
+    });
+
+    req.flush(updateCategory);
+  });
+
+  it('should get the selected category', () => {
+    const mockCategory: Category = {
+      code: 'fitness',
+      order: 0,
+      title: '',
+      active: false,
+    };
+    service.getCategory('fitness').subscribe((res) => {
+      expect(res).toEqual(mockCategory);
+    });
+
+    const req = httpController.expectOne({
+      method: 'GET',
+      url: '/course-api/category?code=fitness',
+    });
+
+    req.flush(mockCategory);
+  });
 });
 
 const mockCategories: jasmine.Expected<jasmine.ArrayLike<Category>> = [
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -16,7 +16,6 @@ export class CategoryService {
   constructor(private http: HttpClient) {}
 
   getCategories(): Observable<Category[]> {
-    //FIXME - Better way of reading the base URL by environment
     return this.http.get<Category[]>(environment.courseUrl + '/category');
   }
 
@@ -34,26 +33,4 @@ export class CategoryService {
       category
     );
   }
-
-  handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      //client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      console.error(
-        `Backend returned code ${error.status}, body was: `,
-        error.error
-      );
-    }
-    // Return an observable with a user-facing error message.
-    return throwError(
-      () => new Error('Something bad happened; please try again later.')
-    );
-  }
-}
-
-function code(arg0: string, code: any): HttpParams {
-  throw new Error('Function not implemented.');
 }
